335941c0bc6d4464bd2effd1b83975a5506eddcf
huangzhiyuan03 <563044462@qq.com>
2026-04-26 21:56:39 +0800

    feat: register /custom-dict route

--- src/pages/options/router.ts
+++ src/pages/options/router.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppOptions from "./AppOptions.vue";
 import AppProviderTypes from "./AppProviderTypes.vue";
 import Privacy from "./Privacy.vue";
+import AppCustomDict from "./AppCustomDict.vue";
 
 const routes = [
     { path: "/", component: AppOptions },
     { path: "/provider-types", component: AppProviderTypes },
-    { path: "/privacy", component: Privacy }
+    { path: "/privacy", component: Privacy },
+    { path: "/custom-dict", component: AppCustomDict },
 ]
 
 const router = createRouter({
